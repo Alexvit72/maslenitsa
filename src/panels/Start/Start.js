592aa8fc47e61b1912ab55979039698217d33add
Alexit72 <aleksandrvitebsk@tut.by>
2021-03-02 12:56:05 +0300

    fix

--- src/panels/Start/Start.js
+++ src/panels/Start/Start.js
@@ -5,7 +5,7 @@ import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import Card from '../../components/Card';
 import logo from '../../img/logo.png';
 import PopoutWrapper from '@vkontakte/vkui/dist/components/PopoutWrapper/PopoutWrapper';
-import sad from '../../img/sad.png'
+
 
 const Start = props => {
 	useEffect(() => {
@@ -30,7 +30,7 @@ const Start = props => {
 		`1. Испытай удачу в нашей игре! Получи возможность выиграть скидочные купоны в Дикси.*`,
 		`2. Чтобы регулировать силу вращения барабана, нажми кнопку «крутить», когда шкала силы заполнится до нужного уровня.`,
 		`3. Ты можешь получить дополнительные попытки. Для этого или кликни по кнопке “+” вверху и выполняй задания, или дождись паузы в 5 часов.`,
-		`4.  В случае выигрыша купон** со скидкой будет отправлен в личные сообщения. Для этого нужно разрешить сообществу Дикси отправлять сообщения. В одной попытке можно выиграть максимум один купон.`,
+		`4. В случае выигрыша купон со скидкой будет отправлен в личные сообщения. Для этого нужно разрешить сообществу Дикси отправлять сообщения. Приступим ?`,
 		`* Организатор вправе прекратить акцию досрочно. Количество купонов ограничено.`
 	].map((item, index) => {
 		return <p className='list-item' key={index}>{item}</p>;
@@ -52,22 +52,13 @@ const Start = props => {
 					inner={<a href='https://dixy.ru/upload/medialibrary/765/Rules.27a4066a.pdf'
 						target="_blank"> Полные правила акции</a>} label='Начать' onClick={start} />
 			</div>
-<<<<<<< HEAD
 			{ props.message ?
-=======
-			{/* {!props.play ?
->>>>>>> 999b2b61786014b726d99ce65eee767b4fabf511
 				<PopoutWrapper alignX='center' alignY='center' className='Send'>
 					<Card className='Card message'
 						title='Упс!'
 						text='Купоны на сегодня закончились. Передохните. Завтра продолжим игру.'
 						label='Закрыть'
-<<<<<<< HEAD
 						//play={true}
-=======
-						img={sad}
-						play={true}
->>>>>>> 999b2b61786014b726d99ce65eee767b4fabf511
 						onClick={closeSend}
 						onClose={closeSend}
 						close={true}
